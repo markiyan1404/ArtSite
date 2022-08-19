5c2d847562d29f3787b55b909625b1a05850cf95
markiyan1404 <markiyan1404@gmai.com>
2022-08-19 17:49:43 +0300

    fix 404

---
+++ dist/js/content_sculptures_content_files_sculptures_json..js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["content_sculptures_content_files_sculptures_json"],{
+
+/***/ "./content/sculptures/content_files/sculptures.json":
+/*!**********************************************************!*\
+  !*** ./content/sculptures/content_files/sculptures.json ***!
+  \**********************************************************/
+/***/ (function(module) {
+
+eval("module.exports = JSON.parse('{\"en\":{\"main_title\":\"Sculptures\",\"sculptureName0\":\"Nefertiti Bust\",\"sculptureName1\":\"Discobolus\",\"sculptureName2\":\"Winged Victory of Samothrace\",\"sculptureName3\":\"Venus de Milo\",\"sculptureName4\":\"Apollo Belvedere\",\"sculptureName5\":\"Laocoön and His Sons\",\"sculptureName6\":\"David\",\"sculptureName7\":\"Little Dancer\",\"sculptureName8\":\"Thinker\",\"sculptureName9\":\"Kiss\",\"year0\":\"1345 ВС\",\"year1\":\"455 ВС\",\"year2\":\"293 ВС\",\"year3\":\"135 BC\",\"year4\":\"130 BC\",\"year5\":\"30 BC\",\"year6\":\"1504\",\"year7\":\"1880\",\"year8\":\"1904\",\"year9\":\"1908\",\"button\":\"More\"},\"ua\":{\"main_title\":\"Скульптури\",\"sculptureName0\":\"Бюст Нефертіті\",\"sculptureName1\":\"Дискобол\",\"sculptureName2\":\"Крилата перемога Самофракії\",\"sculptureName3\":\"Венера Мілоська\",\"sculptureName4\":\"Аполлон Бельведерський\",\"sculptureName5\":\"Лаокоон і його сини\",\"sculptureName6\":\"Давид\",\"sculptureName7\":\"Маленька танцівниця\",\"sculptureName8\":\"мислитель\",\"sculptureName9\":\"Поцілунок\",\"year0\":\"1345 до н.е.\",\"year1\":\"455 ВС\",\"year2\":\"293 ВС\",\"year3\":\"135 до н. е.\",\"year4\":\"130 до н. е.\",\"year5\":\"30 рік до нашої ери\",\"year6\":\"1504\",\"year7\":\"1880\",\"year8\":\"1904\",\"year9\":\"1908\",\"button\":\"Більше\"},\"es\":{\"main_title\":\"Esculturas\",\"sculptureName0\":\"Busto de Nefertiti\",\"sculptureName1\":\"Discobolus\",\"sculptureName2\":\"Victoria alada de Samotracia\",\"sculptureName3\":\"Venus de Milo\",\"sculptureName4\":\"Apolo Belvedere\",\"sculptureName5\":\"Laocoonte y sus hijos\",\"sculptureName6\":\"David\",\"sculptureName7\":\"Pequeña bailarina\",\"sculptureName8\":\"pensador\",\"sculptureName9\":\"El beso\",\"year0\":\"1345 aC\",\"year1\":\"455 ВС\",\"year2\":\"293 ВС\",\"year3\":\"135 aC\",\"year4\":\"130 aC\",\"year5\":\"30 aC\",\"year6\":\"1504\",\"year7\":\"1880\",\"year8\":\"1904\",\"year9\":\"1908\",\"button\":\"Más\"}}');\n\n//# sourceURL=webpack:///./content/sculptures/content_files/sculptures.json?");
+
+/***/ })
+
+}]);
---
+++ dist/js/content_sculptures_sculpture_kiss_json..js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["content_sculptures_sculpture_kiss_json"],{
+
+/***/ "./content/sculptures/sculpture/kiss.json":
+/*!************************************************!*\
+  !*** ./content/sculptures/sculpture/kiss.json ***!
+  \************************************************/
+/***/ (function(module) {
+
+eval("module.exports = JSON.parse('{\"en\":{\"main_title\":\"Kiss\",\"name\":\"Kiss\",\"author\":\"Auguste Rodin\",\"year\":\"1908\",\"description\":\"This sculpture was commissioned by the French government in 1888 for the Exhibition Universelle. The kissing couple were originally depicted as a relief in a portal made by Rodin titled “The Gates of Hell,” along with a figure that became the inspiration for “The Thinker”. The portal was supposed to serve as the entrance to an art museum that was never built.\",\"button\":\"3D\"},\"ua\":{\"main_title\":\"Поцілунок\",\"name\":\"Поцілунок\",\"автор\":\"Огюст Роден\",\"рік\":\"1908\",\"description\":\"Цю скульптуру було замовлено французьким урядом у 1888 році для Всесвітньої виставки. Пара, що цілується, спочатку була зображена як рельєф на створеному Роденом порталі під назвою \\'Ворота пекла\\' разом із фігурою, яка стала натхнення для \\'Мислителя\\'. Портал мав служити входом до художнього музею, який так і не був побудований.\",\"button\":\"3D\"},\"es\":{\"main_title\":\"Beso\",\"name\":\"Beso\",\"author\":\"Augusto Rodin\",\"year\":\"1908\",\"description\":\"Esta escultura fue encargada por el gobierno francés en 1888 para la Exposición Universal. La pareja que se besa se representó originalmente como un relieve en un portal hecho por Rodin titulado \\'Las puertas del infierno\\', junto con una figura que se convirtió en la inspiración para \\'El pensador\\'. Se suponía que el portal serviría como entrada a un museo de arte que nunca se construyó.\",\"button\":\"3D\"},\"specifical\":{\"authorLink\":\"auguste-rodin\"}}');\n\n//# sourceURL=webpack:///./content/sculptures/sculpture/kiss.json?");
+
+/***/ })
+
+}]);
